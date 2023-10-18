d7b9cd096b889a59a74ab9568969c87f91a855b5
hugo-gonzalez <hugo.gonzalez@binmatter.com>
2023-10-18 12:52:10 -0700

    Add object movement

--- Assets/Scripts/Obstacles/ObjectMovement.cs
+++ Assets/Scripts/Obstacles/ObjectMovement.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 
 public class ObjectMovement : MonoBehaviour {
 
+	private void Start() {
+		//Obtain upper border of the screen
+		var mainCamera = Camera.main;
+        var worldPosition   = mainCamera.ViewportToWorldPoint(Vector3.up);
+		var errorRange = 5;
+
+		this.transform.DOMoveY(worldPosition.y + errorRange, 5f).SetEase(Ease.InCubic);
+	}
 
 	public void Move(float finalPosition, float velocity){
-		this.transform.DOMoveX(finalPosition, velocity).SetEase(Ease.InCubic);
 	}
 }
