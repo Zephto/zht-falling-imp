d7df170604593c5adeef527c683b2c3b5c290c63
hugo-gonzalez <hugo.gonzalez@binmatter.com>
2023-10-23 10:02:17 -0700

    Add trigger collision conditions

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,47 +1,35 @@
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public float moveSpeed = 5.0f;
-    private Rigidbody2D rb;
+	public float moveSpeed = 5.0f;
+	private Rigidbody2D rb;
 
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
 
-    private void FixedUpdate()
-    {
-        // // Verifica si el juego se está ejecutando en un dispositivo móvil
-        // if (Application.isMobilePlatform)
-        // {
-        //     // Mueve al jugador usando el acelerómetro
-        //     Vector3 acceleration = Input.acceleration;
-        //     Vector3 movement = new Vector3(acceleration.x, 0, 0);
-        //     rb.AddForce(movement * moveSpeed);
-        // }
-        // else
-        // {
-        //     // Mueve al jugador usando las flechas del teclado en el editor
-        //     float moveHorizontal = Input.GetAxis("Horizontal");
-        //     Vector3 movement = new Vector3(moveHorizontal, 0, 0);
-        //     rb.AddForce(movement * moveSpeed);
-        // }
+	private void FixedUpdate() {
 
-        float moveHorizontal = 0.0f;
+		float moveHorizontal = 0.0f;
 
-        // Verifica si el juego se está ejecutando en un dispositivo móvil
-        if (Application.isMobilePlatform)
-        {
-            // Mueve al jugador usando el acelerómetro
-            moveHorizontal = Input.acceleration.x;
-        }
-        else
-        {
-            // Mueve al jugador usando las flechas del teclado en el editor
-            moveHorizontal = Input.GetAxis("Horizontal");
-        }
+		// Verifica si el juego se está ejecutando en un dispositivo móvil
+		if (Application.isMobilePlatform)
+		{
+			// Mueve al jugador usando el acelerómetro
+			moveHorizontal = Input.acceleration.x;
+		}
+		else
+		{
+			// Mueve al jugador usando las flechas del teclado en el editor
+			moveHorizontal = Input.GetAxis("Horizontal");
+		}
 
-        Vector2 movement = new Vector2(moveHorizontal, 0);
-        rb.velocity = movement * moveSpeed;
-    }
+		Vector2 movement = new Vector2(moveHorizontal, 0);
+		rb.velocity = movement * moveSpeed;
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log("collisione con: " + other.name);
+	}
 }
