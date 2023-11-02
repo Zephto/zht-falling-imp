f68423137b79c59dde46625e06fe67b5340b4544
hugo-gonzalez <hugo.gonzalez@binmatter.com>
2023-11-02 15:49:10 -0700

    Work in spawn diferent types of hell objects

--- Assets/Scripts/Obstacles/ObjectController.cs
+++ Assets/Scripts/Obstacles/ObjectController.cs
@@ -3,23 +3,32 @@ using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 using MyBox;
-using Unity.Mathematics;
+using System;
 
+/// <summary>
+/// Component used to control all the spawned object parameters
+/// </summary>
 [RequireComponent(typeof(BoxCollider2D))]
 public class ObjectController : MonoBehaviour {
 
-	public enum Type{
-		OBSTACLE,
-		COIN
+	public enum Type{ OBSTACLE, MONSTER, COIN, SPECIAL_COIN, ITEM }
+
+	[Serializable]
+	public class HellObjectReferences{
+		public string hellId;
+		public Type hellType;
+		public GameObject hellObject;
+		public ParticleSystem hellParticles;
 	}
 
 	#region Public variables
-	public Type objectType;
-	public float speedGame = 0;
+	public List<HellObjectReferences> objectsReferences;
 	[SerializeField] private GameObject particles;
+	[HideInInspector] public float speedGame = 0;
 	#endregion
 
 	#region Private variables
+	private HellObjectReferences currentSelection;
 	private Camera mainCamera;
 	private float finalPosition;
 	private Tweener moveTween;
@@ -27,6 +36,8 @@ public class ObjectController : MonoBehaviour {
 
 	#region Consts
 	private const float DefaultSpeed = 7f;
+	private const float MonsterSpeed = 5f;
+	private const float ErrorRange = 5f;
 	#endregion
 
 	private void Awake() {
@@ -34,19 +45,40 @@ public class ObjectController : MonoBehaviour {
         finalPosition   = mainCamera.ViewportToWorldPoint(Vector3.up).y;
 	}
 
+	#region On Methods
 	private void OnEnable() {
-		//Obtain upper border of the screen
-		var errorRange = 5;
+		//Set random type
+		SelectRandomType();
 
-		moveTween = this.transform.DOMoveY(finalPosition + errorRange, DefaultSpeed - (speedGame*2)).SetEase(Ease.Linear).OnComplete(()=>{
+		var objectSpeed = (currentSelection.hellType == Type.MONSTER)? MonsterSpeed : DefaultSpeed;
+		var toPos = finalPosition + ErrorRange;
+		var duration = objectSpeed - (speedGame*2);
+		moveTween = this.transform.DOMoveY(toPos, duration).OnComplete(()=>{
 			this.gameObject.SetActive(false);
-		});
+		}).SetEase(Ease.Linear);
+	}
+
+	private void OnDisable() {
+		currentSelection.hellObject.SetActive(false);
 	}
 
 	private void OnTriggerEnter2D(Collider2D other) {
-		Instantiate(particles, this.transform.position, quaternion.identity, this.transform.parent);
+		Instantiate(particles, this.transform.position, Quaternion.identity, this.transform.parent);
 		moveTween.Kill();
 		this.gameObject.SetActive(false);
 	}
+	#endregion
 
+	#region Private Methods
+	private void SelectRandomType(){
+		//En esta parte se puede jugar con las probabilidades, pero ya despues
+		var randomIndex = new System.Random().Next(objectsReferences.Count);
+		currentSelection = objectsReferences[randomIndex];
+		currentSelection.hellObject.SetActive(true);
+	}
+	#endregion
+
+	#region Public Methods
+	
+	#endregion
 }
--- Assets/Scripts/Obstacles/ObjectSpawner.cs
+++ Assets/Scripts/Obstacles/ObjectSpawner.cs
@@ -7,77 +7,66 @@ using System.Linq;
 /// Component used to spawn objects (obstacles and collectables)
 /// </summary>
 public class ObjectSpawner : MonoBehaviour {
-    public GameObject objectPrefab;  // Lista de objetos prefabricados
-    public float spawnInterval = 2.0f;    // Intervalo de generación
-    public float spawnDelay = 2.0f;       // Retraso inicial
+	public GameObject objectPrefab;  // Lista de objetos prefabricados
+	public float spawnInterval = 2.0f;    // Intervalo de generación
+	public float spawnDelay = 2.0f;       // Retraso inicial
 
-    private List<GameObject> objects;      // Lista de objetos generados
-    private float timer;
-    private Camera mainCamera;
-    private bool canSpawn;
+	private List<GameObject> objects;      // Lista de objetos generados
+	private float timer;
+	private Camera mainCamera;
+	private bool canSpawn;
 
-    private void Start()
-    {
-        objects = new List<GameObject>();
-        timer = spawnDelay;
-        mainCamera = Camera.main;
-        canSpawn = false;
+	private void Start()
+	{
+		objects = new List<GameObject>();
+		timer = spawnDelay;
+		mainCamera = Camera.main;
+		canSpawn = false;
 
-        //Move Spawner to bottom of the screen
-        var screenPosition  = new Vector3(Screen.width/2, 0, mainCamera.nearClipPlane);
-        var worldPosition   = mainCamera.ScreenToWorldPoint(screenPosition);
+		//Move Spawner to bottom of the screen
+		var screenPosition  = new Vector3(Screen.width/2, 0, mainCamera.nearClipPlane);
+		var worldPosition   = mainCamera.ScreenToWorldPoint(screenPosition);
 
-        //Set world position to current object
-        this.transform.position = worldPosition;
-    }
+		//Set world position to current object
+		this.transform.position = worldPosition;
+	}
 
-    // private void Update() {
-    //     if(!canSpawn) return;
+	public void SpawnObject(float speed) {
+		var spawnX      = Random.Range(0.1f, 0.9f); // Rango de 0 a 1 en coordenadas de pantalla
+		var errorRange  = 5;
 
-    //     timer -= Time.deltaTime;
-    //     if (timer <= 0)
-    //     {
-    //         SpawnObject();
-    //         timer = spawnInterval;
-    //     }
-    // }
+		Vector3 spawnPosition = mainCamera.ViewportToWorldPoint(new Vector3(spawnX, 1, 0));
+		spawnPosition.y = this.transform.position.y - errorRange;
+		spawnPosition.z = 0;
+		ObjectController objectSelected = null;
 
-    public void SpawnObject(float speed) {
-        var spawnX      = Random.Range(0.2f, 0.8f); // Rango de 0 a 1 en coordenadas de pantalla
-        var errorRange  = 5;
+		if(objects.Count > 10){
+			//Pool existing objects
+			var inactiveObjects = objects.Where(obj => !obj.activeSelf).ToList();
+			if(inactiveObjects.Count > 0){
 
-        Vector3 spawnPosition = mainCamera.ViewportToWorldPoint(new Vector3(spawnX, 1, 0));
-        spawnPosition.y = this.transform.position.y - errorRange;
-        spawnPosition.z = 0;
-        ObjectController objectSelected = null;
+				var randomValue = Random.Range(0, inactiveObjects.Count);
+				var selection   = inactiveObjects[randomValue];
 
-        if(objects.Count > 10){
-            //Pool existing objects
-            var inactiveObjects = objects.Where(obj => !obj.activeSelf).ToList();
-            if(inactiveObjects.Count > 0){
+				objectSelected = selection.GetComponent<ObjectController>();
+			}else{
+				Debug.LogError("There is no more active objects!!!");
+			}
 
-                var randomValue = Random.Range(0, inactiveObjects.Count);
-                var selection   = inactiveObjects[randomValue];
+		}else{
+			//Create new objects
+			GameObject obj = Instantiate(objectPrefab, spawnPosition, Quaternion.identity, this.transform);
+			objectSelected = obj.GetComponent<ObjectController>();
+			objects.Add(obj);
+		}
+		
+		if(objectSelected != null){
+			objectSelected.transform.position = spawnPosition;
+			objectSelected.gameObject.SetActive(true);
+			objectSelected.speedGame = speed;
+		}
 
-                objectSelected = selection.GetComponent<ObjectController>();
-            }else{
-                Debug.LogError("There is no more active objects!!!");
-            }
-
-        }else{
-            //Create new objects
-            GameObject obj = Instantiate(objectPrefab, spawnPosition, Quaternion.identity, this.transform);
-            objectSelected = obj.GetComponent<ObjectController>();
-            objects.Add(obj);
-        }
-        
-        if(objectSelected != null){
-            objectSelected.transform.position = spawnPosition;
-            objectSelected.gameObject.SetActive(true);
-            objectSelected.speedGame = speed;
-        }
-
-    }
+	}
 }
 
 
